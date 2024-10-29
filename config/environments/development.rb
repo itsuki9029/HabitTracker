92fb0e30ca8be2dd46ab422812cdd82582463cf5
林稜大 <hayashiryota@hayashiryoudainoMacBook-Pro.local>
2024-10-29 18:09:43 +0900

    デプロイできていない

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "habit-tracker-broken-cherry-4621.fly.dev"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -22,7 +23,6 @@ Rails.application.configure do
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
