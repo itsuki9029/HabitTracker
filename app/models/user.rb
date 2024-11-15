3a82c926391f736ee31ea4d93185347b3a53dd35
林稜大 <hayashiryota@hayashiryoudainoMacBook-Pro.local>
2024-11-16 06:58:48 +0900

    プロフィール編集機能実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :habits, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  mount_uploader :profile_image, ProfileImageUploader
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
