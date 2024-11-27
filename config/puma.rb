99a163109e7519cdbbc659369f039ead2b4df39d
林稜大 <hayashiryota@hayashiryoudainoMacBook-Pro.local>
2024-11-27 09:59:10 +0900

    ぷま

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 port ENV.fetch("PORT") { 3000 }
 
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
