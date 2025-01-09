bd34d5915b3dd80747c83de9a7bf493019cba98a
林稜大 <hayashiryota@hayashiryoudainoMacBook-Pro.local>
2025-01-09 17:18:09 +0900

    お問い合わせ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'comments/create'
+  get '/contact', to: 'static_pages#contact'
+  post '/contact', to: 'static_pages#create_contact'
+  get '/terms', to: 'static_pages#terms'
+  get '/privacy', to: 'static_pages#privacy'
   root 'home#index'
   devise_for :users, controllers: {
     omniauth_callbacks: 'omniauth_callbacks'
